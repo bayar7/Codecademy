e73ff38785b20178f86c2e40c033eb2260f9e10e
Bayar OneZero <stereotronic7@gmail.com>
2022-08-22 21:05:22 +0800

    Cute little code added

---
+++ Coding/Codecademy/JavaScript/truthyOrFalsy.js
@@ -0,0 +1,7 @@
+// Cute little line of code that takes in any value and 
+//returns true if that value is truthy and false 
+//if that value is falsy.
+
+const truthyOrFalsy = value => value ? true : false 
+
+console.log(truthyOrFalsy(0))
